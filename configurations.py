d3969a401711c5603f413a73b3016d1d1dbecc9c
DynowskiKonrad <224074@edu.p.lodz.pl>
2018-12-20 18:09:15 +0100

    Added config management with yaml

--- configurations.py
+++ configurations.py
@@ -1,4 +1,7 @@
-def getconfig(configfilename="config.txt"):
+import yaml
+
+
+def getconfig(configfilename="config.yml"):
     """
     The configs have default values of size and symbol
     If in config file one value is present, then it added on right place in configuration dict
@@ -7,21 +10,6 @@ def getconfig(configfilename="config.txt"):
     :return:
         - Returns dictionary compatible with the plotting function
     """
-    configs ={
-        "xsize": 16,
-        "ysize": 9,
-        "symbol": 'x',
-        "nameofinput": 'data.txt',
-        "nameofvalue": 'Wavelength $\lambda$[nm]',
-        "nameofarg": "argument $x$",
-        "nameofoutput": '0',
-        "showlegend": '0'
-    }
     with open(configfilename, 'r') as f:
-        for line in f.readlines():
-            x, y = line.split(' ')
-            if x == "xsize" or x == "ysize":
-                configs[x] = float(y)
-            elif x == "symbol":
-                configs[x] = y
+        configs = yaml.load(f)
     return configs
--- datacreator.py
+++
@@ -1,7 +0,0 @@
-import random
-import numpy
-
-with open("data.txt", 'w') as f:
-    arr = numpy.array([(random.randint(i, 1000), random.randint(1, 1000 * (1000 - i))/100) for i in range(1000)])
-    for datapoint in arr:
-        print("{a} {b}".format(a=datapoint[0], b=datapoint[1]), end="\n", file=f)
